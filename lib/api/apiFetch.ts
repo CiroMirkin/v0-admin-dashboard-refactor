fb2afeb0f0db281c823a983d313add0f8975ccba
Thiago Maneyro <tmaneyro@gmail.com>
2026-01-31 19:06:25 -0300

    front v2

--- lib/api/apiFetch.ts
+++ lib/api/apiFetch.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || ''
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || ''
 
 interface ApiFetchOptions extends Omit<RequestInit, 'body'> {
   body?: Record<string, unknown> | FormData | string
